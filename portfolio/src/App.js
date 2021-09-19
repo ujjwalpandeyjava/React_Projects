111298b3f0efb904bfb51ff87726c1cbd6f66a36
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2021-09-19 21:42:29 +0530

    About me section ready, minute touch remains.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import floatingImage from './images/ujjwalAvatar1Copy.png'
+import aboutImage from './images/ujjwalAvatar3.png'
 import myPicture from './images/IMG-20210325-WA0006.jpg'
 import './App.css';
 
@@ -28,23 +29,23 @@ function App() {
     <div className="socialIcons">
       <div className="icon">
         <div className="tooltip">Facebook</div>
-        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noopener"><i className="fab fa-facebook-f"></i></a></span>
+        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a></span>
       </div>
       <div className="icon">
         <div className="tooltip">Twitter</div>
-        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noopener"><i className="fab fa-twitter"></i></a></span>
+        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></span>
       </div>
       <div className="icon">
         <div className="tooltip">LinkedIn</div>
-        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noopener"><i className="fab fa-linkedin"></i></a></span>
+        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a></span>
       </div>
       <div className="icon">
         <div className="tooltip">Instagram</div>
-        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noopener"><i className="fab fa-instagram"></i></a></span>
+        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a></span>
       </div>
       <div className="icon">
         <div className="tooltip">Github</div>
-        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noopener"><i className="fab fa-github"></i></a></span>
+        <span><a href="https://ujjwalpandeyjava.github.io/Links/" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></span>
       </div>
     </div>
   const navbar = <Fragment>
@@ -88,12 +89,57 @@ function App() {
       </section>
     </div>
   </Fragment>
-
+  const aboutSection = <Fragment>
+    <div id="aboutSection">
+      <h1>About me</h1>
+      <div id="firstInAbout">
+        <div>
+          <img src={aboutImage} />
+        </div>
+        <div id="aboutMe">
+          <div>
+            <div>My name is Ujjwal Pandey, pursuing graduation in:<br /><span> BCA</span> from<span> IGNOU</span>
+              and <span>GNIIT</span> from<span> NIIT</span><br /><br />
+              I am a full stack Web Developer,
+              I work with Java, Advanced-Java, JavaScript, React, MySQL, MongoDB, and so on... according to the projects I am working on.
+              <br /><br />My <strong>"Area of Interest"</strong> includes "Full-stact Web app Development" and Learning technology.
+              I am looking to connect with engeineers and a company to work with on same field of interest.
+            </div>
+            <div id="downloadCV"><a href="#">Download CV</a></div>
+          </div>
+          <div id="bars">
+            <div style={{ fontWeight: "bold" }}><p>Want to know how good I am?</p><hr/></div>
+            <div>
+              <p><span>Back-end Development</span><span>90%</span></p>
+              <section>---------------------------------------------</section>
+            </div>
+            <div>
+              <p>Front-end Development</p>
+              <section>---------------------------------------------</section>
+            </div>
+            <div>
+              <p>ReactJs app Development</p>
+              <section>---------------------------------------------</section>
+            </div>
+            <div>
+              <p> Full-Stack app Development</p>
+              <section>---------------------------------------------</section>
+            </div>
+            <div>
+              <p>Competative Coding</p>
+              <section>---------------------------------------------</section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Fragment>
   return (
     <Fragment>
       {navbar}
       <div className="main">
         {firstView}
+        {aboutSection}
         {/* <div style="display: block; height: 30px;"></div> */}
         <div className="about">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis molestiae tenetur, et
           pariatur dignissimos qui at harum eligendi iste optio, cum voluptas quam. Assumenda at tenetur, numquam
