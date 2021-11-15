4a21d253d3c2ae208b86d0848aa9d238262446e5
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2021-11-15 10:58:41 +0530

    First commit of pokedictionary.

---
+++ poke-dictionary/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/header'
+function App() {
+  return (
+    <div className="App">
+      Create the pokeDictionary app.
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ poke-dictionary/src/components/header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Fragment } from "react/cjs/react.production.min";
+// import '/cssFiles/header.css'
+import './cssFiles/header.css'
+
+
+function Header(props) {
+    return (
+        <Fragment>
+            <div className="navbar vertical-content-center">
+                <div className="navbar_inner">
+                    <div class="nav-header">
+                    JoGeek
+                        {/* <div class="nav-title"></div> */}
+                    </div>
+                    <div className="navbar_links">
+                        ul | ul | ul | ul
+                    </div>
+                    {/* Hi, I am Header of the page. */}
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+
+export default Header
